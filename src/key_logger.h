fdb1f6f217ce51841396201440921a82079b0c7a
居戎氏 <chen.sst@gmail.com>
2025-06-07 11:58:33 +0800

    New plugin: keylogger

---
+++ src/key_logger.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <rime/common.h>
+#include <rime/context.h>
+#include <rime/engine.h>
+#include <rime/processor.h>
+
+using namespace rime;
+
+class KeyLogger : public Processor {
+ public:
+  explicit KeyLogger(const Ticket& ticket)
+    : Processor(ticket) {
+    Context* context = engine_->context();
+    update_connection_ = context->update_notifier()
+      .connect([this](Context* ctx) { OnUpdate(ctx); });
+  }
+
+  virtual ~KeyLogger() {
+    update_connection_.disconnect();
+  }
+
+  ProcessResult ProcessKeyEvent(const KeyEvent& key_event) override {
+    return kNoop;
+  }
+
+ private:
+  void OnUpdate(Context* ctx) {}
+
+  connection update_connection_;
+};
